fcd0b015a2ff11666b91490a70dbfabcbb172c71
WhiteHunter2000 <andrewkola13@gmail.com>
2024-10-15 13:07:46 -0500

    RA App
    This is an app that is used to help RA's at AUM. It consists of all the information and forms that an RA uses on a daily basis.

---
+++ service-wroker.js
@@ -0,0 +1,29 @@
+const CACHE_NAME = 'ra-app-cache-v1';
+const urlsToCache = [
+    '/',
+    '/index.html',
+    '/forms.html',
+    '/health.html',
+    '/information.html',
+    '/styles.css',
+    '/icon-192x192.png',
+    '/icon-512x512.png',
+];
+
+self.addEventListener('install', event => {
+    event.waitUntil(
+        caches.open(CACHE_NAME)
+            .then(cache => {
+                return cache.addAll(urlsToCache);
+            })
+    );
+});
+
+self.addEventListener('fetch', event => {
+    event.respondWith(
+        caches.match(event.request)
+            .then(response => {
+                return response || fetch(event.request);
+            })
+    );
+});
